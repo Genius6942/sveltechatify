664216839be5048d465cb16ec99625d1088fa8b5
Joshua Liu <mathmanjosh@gmail.com>
2023-08-28 13:44:40 -0400

    save

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import "./app.css";
+
+  import { SignedIn, SignedOut, Doc, Collection } from "sveltefire";
+	
+  import { signInAnonymously } from "firebase/auth";
+</script>
+
+
+<FirebaseApp {auth} {firestore}>
+	
+</FirebaseApp>
